e5c4c4f5d4d060272dd236bb51b95656c1000ebb
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-12-12 13:06:11 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,5 +23,6 @@ end
 
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.find { |cheese_types| cheese_types.length > 5 }
+  if  cheese_types.find { |cheese_types| cheese_types.length > 5 }
+  else
 end
